f360f466cda3d6ef1a35b21a3dca68b6f9f641e2
Leonid Azackii <azatsckiileon999@gmail.com>
2024-12-20 12:53:11 +0300

    init project

---
+++ src/main/java/ru/sfedu/ictis/sports_sections/SportsSectionsApplication.java
@@ -0,0 +1,13 @@
+package ru.sfedu.ictis.sports_sections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsSectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsSectionsApplication.class, args);
+	}
+
+}
